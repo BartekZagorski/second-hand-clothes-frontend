64975e320883ceedfe9e3ddbb5a210d497d4d514
zagora17 <zagora171991@gmail.com>
2023-03-09 05:11:56 +0100

    Add new class SuperCategory

---
+++ src/app/common/super-category.spec.ts
@@ -0,0 +1,7 @@
+import { SuperCategory } from './super-category';
+
+describe('SuperCategory', () => {
+  it('should create an instance', () => {
+    expect(new SuperCategory()).toBeTruthy();
+  });
+});
---
+++ src/app/common/super-category.ts
@@ -0,0 +1,7 @@
+export class SuperCategory {
+
+    constructor(public id: number,
+        public name: string) {
+
+    }
+}
