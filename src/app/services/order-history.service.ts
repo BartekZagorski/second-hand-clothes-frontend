c77ee38833f8d3d7ea3067e929eef012aa7d2070
zagora17 <zagora171991@gmail.com>
2023-06-09 07:23:10 +0200

    create oreder history service
    Create service that handle fetching orders from the database that are
    associated with logged in user

---
+++ src/app/services/order-history.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from '../common/order';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHistoryService {
+
+  private orderUrl = "http://localhost:8080/api/orders";
+
+  constructor(private httpClient: HttpClient) { 
+    
+  }
+
+  getOrderHistory(theEmail: string): Observable<GetResponseOrderHistory> {
+
+    const orderHistoryUrl = this.orderUrl + "/search/findByCustomerEmail?email=" + theEmail;
+
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
+  }
+}
+
+interface GetResponseOrderHistory {
+  _embedded: {
+    orders: Order[];
+  }
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+    }
+}
