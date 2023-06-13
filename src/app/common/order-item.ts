95b10c6ad5c6586fd134fd454cd19d848a485603
zagora17 <zagora171991@gmail.com>
2023-06-13 05:36:01 +0200

    add name property
    Inside OrderItem class add name property

--- src/app/common/order-item.ts
+++ src/app/common/order-item.ts
@@ -3,11 +3,13 @@ import { CartItem } from "./cart-item";
 export class OrderItem {
     
     imageUrl: string;
+    name: string;
     unitPrice: number;
     productId: string;
 
 constructor(item: CartItem) {
     this.imageUrl = item.imageUrl;
+    this.name = item.name;
     this.unitPrice = item.unitPrice;
     this.productId = item.id;
 }
